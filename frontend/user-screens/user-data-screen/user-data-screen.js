cdbd6795c82ad8667c474ab864849191be6c7e78
Luan1611 <luan_1611@hotmail.com>
2024-11-08 21:51:37 -0300

    feat: getUserRegistrationData function updated

--- frontend/user-screens/user-data-screen/user-data-screen.js
+++ frontend/user-screens/user-data-screen/user-data-screen.js
@@ -1,9 +1,56 @@
 const userCardDiv = document.querySelector('.user-card')
-const CPFParagraph = document.querySelector('#cpf')
-
 const form = document.querySelector('[data-form="user"]')
 
-form.addEventListener('submit', async e => {
+const getUserRegistrationData = async () => {
+
+    const cpf = localStorage.getItem('cpf')
+
+    //TODO: o login é POST ou PUT?
+    //TODO: pegar o cpf a partir do login realizado (servidor terá que retornar o cpf após o login)
+    //pesqusiar
+
+    try {
+        e.preventDefault()
+
+        const cpf = CPFParagraph.textContent
+        const response = await fetch(`http://localhost/project-towing-services-system-backend/client/client-registration-data/${cpf}`)
+        
+        if (!response.ok) {
+            const errorObj = await response.json()
+            console.log(errorObj)
+            throw new Error(errorObj.msg)
+        }
+
+        const userRegistrationData = await response.json()
+
+        //TODO: preencher card com as informações pessoais
+        //questão: de onde virá o email
+
+        const cpfParagraph = document.createElement('p')
+        // cpfParagraph.setAttribute('data-cpf', 'cpf')
+
+        const nameParagraph = document.querySelector('p')
+        // nameParagraph.setAttribute('data-name', 'nome')
+
+        const phoneParagraph = document.querySelector('p')
+        // phoneParagraph.setAttribute('data-phone', 'telefone')
+
+        const emailParagraph = document.querySelector('p')
+
+        cpfParagraph.textContent = userRegistrationData.cpf
+        nameParagraph.textContent = userRegistrationData.nome
+        phoneParagraph.textContent = userRegistrationData.telefone
+        emailParagraph.textContent = userRegistrationData.email
+
+    } catch(err) {
+        console.log(err.message)
+    }
+    
+
+}
+
+
+const fetchPutRequest = async e => {
 
     try {
         e.preventDefault()
@@ -28,11 +75,15 @@ form.addEventListener('submit', async e => {
 
         const newUserData = await response.json()
 
+        //TODO: atualizar card com novos dados cadastrais
     
 
     } catch(err) {
         console.log(err.message)
     }
     
+}
 
-})
+getUserRegistrationData()
+
+form.addEventListener('submit', fetchPutRequest)
