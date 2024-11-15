256f82cafb3b32852d91a42a2a9f79112624e13b
lumonteiro0 <luxo11111@gmail.com>
2024-11-15 16:52:22 -0300

    feat: mais alterações

--- frontend/user-screens/schedule-screen/schedule.js
+++ frontend/user-screens/schedule-screen/schedule.js
@@ -3,7 +3,7 @@
 const schedulingsContainer = document.querySelector('.src-container')
 
 const getServices = async () => {
-
+    console.log('ohniashiohioasciaiojc')
     try {
 
         const response = await fetch(`http://localhost/project-towing-services-system/backend/serviceController.php`)
