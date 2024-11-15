c39b7acce2c5caa30df7b70183b541f7d222b9f5
Luan1611 <luan_1611@hotmail.com>
2024-11-15 16:54:00 -0300

    refactor: some lines deleted in schedule.js

--- frontend/user-screens/schedule-screen/schedule.js
+++ frontend/user-screens/schedule-screen/schedule.js
@@ -1,5 +1,3 @@
-// import availableServices from "../../list.js";
-
 const schedulingsContainer = document.querySelector('.src-container')
 
 const getServices = async () => {
@@ -26,18 +24,3 @@ const getServices = async () => {
 }
 
 getServices()
-
-
-// for (let obj of availableServices) {
-
-//     const servicesDiv = document.createElement('div')
-
-//     let template = `<h4>${obj.serviceDate}</h4>`
-//     for (let service of obj.services) {
-//         template += `<h4>${service.serviceType}: ${service.serviceQuantity}</h4>`
-//     }
-
-//     servicesDiv.innerHTML = template
-
-//     schedulingsContainer.append(servicesDiv)
-// }
