cae4a0dcca516984d128045b6386d9418b4a2a34
Luan1611 <luan_1611@hotmail.com>
2024-11-15 21:42:38 -0300

    feat: i dont know

--- frontend/admin-screens/add-service-screen/service.js
+++ frontend/admin-screens/add-service-screen/service.js
@@ -1,4 +1,5 @@
 const divContainer = document.querySelector('.services-container')
+const btn = document.querySelector('#add-bt')
 
 const getServicesData = async () => {
     try {
@@ -26,3 +27,43 @@ const getServicesData = async () => {
         console.log(err.message)
     }
 }
+
+
+const postService = async (codigo, tipo, preco) => {
+    try {
+
+        let options = {
+            method: "POST",
+                codigo,
+                tipo,
+                preco
+        }
+
+        const response = await fetch(`http://localhost/project-towing-services-system/backend/serviceController.php`, options)
+        
+        if (!response.ok) {
+            const errorObj = await response.json()
+            console.log(errorObj)
+            throw new Error(errorObj.msg)
+        }
+
+        const servicesData = await response.json()
+
+        console.log(servicesData)
+
+        servicesData.forEach(item => console.log(item))
+
+        // servicesData.forEach(item => {
+        //     let innerDiv = document.createElement('div')
+        //     innerDiv.innerHTML = `<p>${}</p><p>${}</p><p>${}</p><p>${}</p><p>${}</p>`
+            
+        // })
+
+    } catch(err) {
+        console.log(err.message)
+    }
+}
+
+btn.addEventListener('click', postService)
+
+getServicesData()
