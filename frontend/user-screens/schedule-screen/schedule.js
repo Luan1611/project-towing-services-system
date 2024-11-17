02187ccd964769c6004e37df0bb19bcbc498ac69
Luan1611 <luan_1611@hotmail.com>
2024-11-17 19:56:03 -0300

    feat: getServices function

--- frontend/user-screens/schedule-screen/schedule.js
+++ frontend/user-screens/schedule-screen/schedule.js
@@ -15,40 +15,63 @@ const getServices = async () => {
 
         const resultado = Object.values(
             servicesData.reduce((acc, item) => {
-              const { data_oferta_servico, tipo, preco, quantidade, codigo } = item;
-          
-              if (!acc[data_oferta_servico]) {
-                acc[data_oferta_servico] = {
-                  data: data_oferta_servico,
-                  servicos: {}
-                };
-              }
-          
-              if (!acc[data_oferta_servico].servicos[tipo]) {
-                acc[data_oferta_servico].servicos[tipo] = [];
-              }
-          
-              acc[data_oferta_servico].servicos[tipo].push({ preco, quantidade, codigo });
-          
-              return acc;
+                const { data_oferta_servico, tipo, preco, quantidade, codigo } = item;
+
+                if (!acc[data_oferta_servico]) {
+                    acc[data_oferta_servico] = {
+                        data: data_oferta_servico,
+                        servicos: {}
+                    };
+                }
+
+                if (!acc[data_oferta_servico].servicos[tipo]) {
+                    acc[data_oferta_servico].servicos[tipo] = [];
+                }
+
+                acc[data_oferta_servico].servicos[tipo].push({ preco, quantidade, codigo });
+
+                return acc;
             }, {})
-          );
-          
-          const finalResultado = resultado.map(({ data, servicos }) => ({
+        );
+
+        const finalResultado = resultado.map(({ data, servicos }) => ({
             data,
             servicos: Object.entries(servicos).map(([tipo, detalhes]) => ({
-              tipo,
-              detalhes
+                tipo,
+                detalhes
             }))
-          }));
-          
-          console.log(JSON.stringify(finalResultado, null, 2));
+        }));
 
-        // servicesData.forEach(item => {
-        //     let innerDiv = document.createElement('div')
-        //     innerDiv.innerHTML = `<p>${}</p><p>${}</p><p>${}</p><p>${}</p><p>${}</p>`
-            
-        // })
+        console.log(JSON.stringify(finalResultado, null, 2));
+
+        finalResultado.forEach(obj => {
+            const cardDiv = document.createElement('div')
+            cardDiv.classList.add('card')
+            const date = document.createElement ('h3')
+            date.textContent = obj.data
+            cardDiv.appendChild(date)
+    
+            obj.servicos.forEach(innerObj => {
+                const h5 = document.createElement ('h5')
+                h5.textContent = innerObj.tipo
+                cardDiv.appendChild(h5)
+    
+                innerObj.detalhes.forEach(detailsObj => {
+                    const pCodigo = document.createElement ('p')
+                    const pQuantidade = document.createElement ('p')
+                    const pPreco = document.createElement ('p')
+                    const br = document.createElement ('br')
+                    pCodigo.textContent = detailsObj.codigo
+                    pQuantidade.textContent = detailsObj.quantidade
+                    pPreco.textContent = detailsObj.preco
+                    cardDiv.appendChild(pCodigo)
+                    cardDiv.appendChild(pPreco)
+                    cardDiv.appendChild(pQuantidade)
+                    cardDiv.appendChild(br)
+                })
+            })
+            schedulingsContainer.appendChild(cardDiv)
+        })
 
     } catch(err) {
         console.log(err.message)
